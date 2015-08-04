83cfbb748a12ea3d8761e3c02201de10b91b3327
Andrea Mckenzie <ma3east@gmail.com>
2015-08-04 21:30:01 +0100

    trying to get event views right for different users

--- eStub_app/config/environment.rb
+++ eStub_app/config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#trying to change dates format
+my_date_formats = { :default => '%d/%m/%Y' } 
+Time::DATE_FORMATS.merge!(my_date_formats) 
+Date::DATE_FORMATS.merge!(my_date_formats)
