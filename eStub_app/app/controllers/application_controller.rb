ff54e2738a7228a4446295a366f9f6e7bcc3e52b
Andrea Mckenzie <ma3east@gmail.com>
2015-08-05 01:39:19 +0100

    resolved that error on appController using some code found on stack, but now wondering why user still shows in index

--- eStub_app/app/controllers/application_controller.rb
+++ eStub_app/app/controllers/application_controller.rb
@@ -6,9 +6,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
+
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
     
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    
+    rescue ActiveRecord::RecordNotFound
+      session[:user_id] = nil # or reset_session
+
   end
 
   def logged_in?
