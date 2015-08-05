bcc1a57059c179bac223809fa303bfa99fa0aee6
Andrea Mckenzie <ma3east@gmail.com>
2015-08-05 01:21:59 +0100

    delete now works as a button, but error, as id no longer exists so doesn't know what to display on home page, trying to resolve

--- eStub_app/app/controllers/application_controller.rb
+++ eStub_app/app/controllers/application_controller.rb
@@ -6,11 +6,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      false 
-    end
+    
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    
   end
 
   def logged_in?
